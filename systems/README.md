fefe3e52d2ae06b8027c1f9307d4f9e6ee55ca5d
Jason Taylor <jason@jasongtaylor.com>
2022-05-09 02:49:23 -0400

    Add parameters to server script

--- systems/README.md
+++ systems/README.md
@@ -9,5 +9,5 @@ wget --no-cache -qO - https://raw.githubusercontent.com/taylor-training/proxmox/
 ## Ubuntu Server
 
 ```bash
-wget --no-cache -qO - https://raw.githubusercontent.com/taylor-training/proxmox/main/systems/ubuntu-server.sh | sudo bash
+wget --no-cache -qO - https://raw.githubusercontent.com/taylor-training/proxmox/main/systems/ubuntu-server.sh | sudo bash -s hostname host-ip
 ```
--- systems/ubuntu-server.sh
+++ systems/ubuntu-server.sh
@@ -6,6 +6,9 @@ NET_GATEWAY="192.168.50.1"
 NET_DEVICE_NAME="ens18"
 ### END VARS ###
 
+SERVER_NAME=$1
+SERVER_IP=$2
+
 ME=`whoami`
 
 if [ "$ME" != "root" ]; then 
@@ -13,7 +16,19 @@ if [ "$ME" != "root" ]; then
     exit 1
 fi
 
-hostnamectl set-hostname ubuntu-server
+hostnamectl set-hostname ${SERVER_NAME}
+
+cat << EOF > /etc/netplan/80-networking-config.yaml
+network:
+    ethernets:
+        ${NET_DEVICE_NAME}:
+            dhcp4: false
+            addresses: [${SERVER_IP}/24]
+            gateway4: ${NET_GATEWAY}
+            nameservers:
+              addresses: [${DNS_IP},8.8.8.8,8.8.4.4]
+    version: 2
+EOF
 
 apt-get update -y
 apt-get upgrade -y
